10931f658949ef5abfb8468af7ddfc2b52d4770e
Blake S. Grant <bsaveranceg@outlook.com>
2017-05-07 10:13:58 -0400

    5_7_17 adjustments

--- app/controllers/epicenter_controller.rb
+++ app/controllers/epicenter_controller.rb
@@ -1,13 +1,37 @@
 class EpicenterController < ApplicationController
+ 
   def feed
+  	@following_tweets = []
+    @tweets = Tweet.all
+    @tweets.each do |tweet|
+      current_user.following.each do |f|
+        if tweet.user_id == f
+          @following_tweets.push(tweet)
+        end
+      end
+    end
   end
 
   def show_user
+  	@user = User.find(params[:id])
   end
 
   def now_following
+  	@user = User.find(params[:follow_id])
+    current_user.following.push(params[:follow_id].to_i)
+    current_user.save
+    redirect_to user_profile_path(id: @user.id)
   end
 
   def unfollow
+  	@user = User.find(params[:unfollow_id])
+    current_user.following.delete(params[:unfollow_id].to_i)
+    current_user.save
+    redirect_to user_profile_path(id: @user.id)
   end
+
+
+  def js_practice
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
-  get 'epicenter/feed'
+	get 'user_profile' => 'epiceneter#show_user'
 
-  get 'epicenter/show_user'
-
-  get 'epicenter/now_following'
-
-  get 'epicenter/unfollow'
+	get 'js_practice' => 'epicenter#js_practice'
+	
+	post 'now_following' => 'epicenter#now_following'
+	
+	post 'unfollow' => 'epicenter#unfollow'
 
   resources :tweets
   devise_for :users
 
-  root 'tweets#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'epicenter#feed'
+
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
